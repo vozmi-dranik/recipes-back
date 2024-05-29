55b612449e9c4e90fdace7b78a79c0ff5daded19
MDz1985 <m_d@tut.by>
2024-05-29 11:41:01 +0200

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+
+const corsOptions: CorsOptions = {
+  origin: ['http://localhost:4200', 'http://localhost:3000'],
+};
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter()
   );
+  app.enableCors(corsOptions);
+
   await app.listen(3000, '0.0.0.0');
 }
 
